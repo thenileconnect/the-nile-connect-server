2179fddd4942fae164efafbd092021772f88eac2
yeabalex <yeabalex18@gmail.com>
2024-11-07 03:02:40 -0800

    first commit

---
+++ src/test/java/com/nileconnect/nileconnectserver/NileconnectserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nileconnect.nileconnectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NileconnectserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
